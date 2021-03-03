8a27c4bbb0cd0b027cd3c9bd0fb262bfc8852f3e
Benjamin Rood <benisrood@gmail.com>
2021-03-03 12:29:53 +0200

    Add __resolveType generation implementation fallback
    Change __resolveType generation for all interface/union types to support
    a fallback option with the `__typename` value if `FRAGMENT_TYPE` is not
    found.

--- src/augment/resolvers.js
+++ src/augment/resolvers.js
@@ -100,20 +100,22 @@ export const augmentResolvers = ({
 
   // must implement __resolveInfo for every Interface type
   // we use "FRAGMENT_TYPE" key to identify the Interface implementation
-  // type at runtime, so grab this value
-  // const derivedTypes = Object.keys(generatedTypeMap).filter(
-  //   e =>
-  //     generatedTypeMap[e].kind === 'InterfaceTypeDefinition' ||
-  //     generatedTypeMap[e].kind === 'UnionTypeDefinition'
-  // );
-  // derivedTypes.map(e => {
-  //   resolvers[e] = {};
+  // type at runtime, falling back on "__typename" attribute if not found
+  const derivedTypes = Object.keys(generatedTypeMap).filter(
+    e =>
+      generatedTypeMap[e].kind === 'InterfaceTypeDefinition' ||
+      generatedTypeMap[e].kind === 'UnionTypeDefinition'
+  );
+  derivedTypes.map(e => {
+    resolvers[e] = {};
 
-  //   resolvers[e]['__resolveType'] = (obj, context, info) => {
-  //     return obj['FRAGMENT_TYPE'];
-  //   };
-  // });
-  // DISABLED FOR THIS FORK, DON'T WANT FORCED __resolveType GENERATION
+    resolvers[e]['__resolveType'] = (obj, context, info) => {
+      const typename = obj['FRAGMENT_TYPE']
+        ? obj['FRAGMENT_TYPE']
+        : obj['__typename']; // falls back to optionally-defined `__typename` attribute/field
+      return typename;
+    };
+  });
 
   return resolvers;
 };
